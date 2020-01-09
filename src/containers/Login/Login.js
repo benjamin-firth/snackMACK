9083917fd815e7490294224a7893c606b8162fdf
Ben Firth <bfirth93@gmail.com>
2020-01-09 14:38:08 -0700

    Create login and add styling

---
+++ src/containers/Login/Login.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './Login.scss';
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      city: null
+    }
+  }
+
+  render() {
+
+    return (
+      <section className='login'>
+        <h1>snackMACK</h1>
+        <h3>“The food truck locator app to bring your dates to the next level!”</h3>
+        <form>
+          <select>
+            <option value='vancouver' >Vancouver</option>
+            <option value='calgary' >Calgary</option>
+          </select>
+          <button>SUBMIT</button>
+        </form>
+      </section>
+    )
+  }
+}
+
+export default Login;
