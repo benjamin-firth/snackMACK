abee4920c34125fe8f701991e9e495eec1702e66
Ben Firth <bfirth93@gmail.com>
2020-01-09 15:30:26 -0700

    Create truck file and skeleton

---
+++ src/components/Truck/Truck.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Truck = () => {
+
+  return (
+    <article>
+      <h4>NAME</h4>
+      <p>This is a description of the food truck</p>
+    </article>
+  )
+}
+
+export default Truck;
