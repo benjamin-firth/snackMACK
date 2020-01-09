4bd6d6ee227c32a6c6d54cc78710abb0d6ef3036
Ben Firth <bfirth93@gmail.com>
2020-01-09 15:31:01 -0700

    Create header files and skeleton

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+
+  return (
+    <header>
+      <h2>snackMack</h2>
+      <h3>Food Nearby</h3>
+    </header>
+  )
+}
+
+export default Header;
