9b04cfad48f9aa042b7f14f1beb414840c61fb75
Ben Firth <bfirth93@gmail.com>
2020-01-09 15:30:43 -0700

    Create trucksidebar files and skeleton

---
+++ src/components/TruckSidebar/TruckSidebar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './TruckSidebar.scss';
+
+class TruckSidebar extends Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <section>
+        <button>VIEW FAVORITE LIST</button>
+      </section>
+    )
+  }
+}
+
+export default TruckSidebar;
