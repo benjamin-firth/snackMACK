a92274757f53bfac076f9e22836f5e0a59710d2f
Ben Firth <bfirth93@gmail.com>
2020-01-10 07:38:58 -0700

    Style truck container

--- src/components/TrucksContainer/TrucksContainer.js
+++ src/components/TrucksContainer/TrucksContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './TruckSidebar.scss';
+import './TrucksContainer.scss';
 import Header from '../Header/Header';
 import Truck from '../Truck/Truck';
 
@@ -12,23 +12,44 @@ class TruckSidebar extends Component {
   }
 
   componentDidMount() {
-    fetch('http://data.streetfoodapp.com/1.1/schedule/calgary')
+    fetch('https://cors-anywhere.herokuapp.com/http://data.streetfoodapp.com/1.1/schedule/calgary')
       .then(res => res.json())
       .then(data => {
         const vendors = data.vendors;
         const vendorKeys = Object.keys(vendors);
         const nearbyTrucks = vendorKeys.map(key => {
-          return <Truck name={vendors[key].name} desc={vendors[key].description} />
+          return { 
+            name: vendors[key].name, 
+            desc: vendors[key].description,
+            location: vendors[key].last,
+            image: vendors[key].images
+          }
         });
         this.setState({ nearbyTrucks })
       })
   }
 
   render() {
+    const Trucks = this.state.nearbyTrucks.map(truck => {
+      return (
+        <Truck 
+          name={truck.name} 
+          desc={truck.desc} 
+          location={truck.location}
+          image={truck.image}
+          key={truck.name}
+        />) 
+    })
+
     return (
       <main>
         <Header />
-        <button>VIEW FAVORITE LIST</button>
+        <div className='sidebar'>
+          <button>VIEW FAVORITE LIST</button>
+          <div className='inner-scroll-container'>
+            {Trucks}
+          </div>
+        </div>
       </main>
     )
   }
