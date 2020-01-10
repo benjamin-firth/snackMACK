69210ec4875fbf4b270c1669e3b40dbfb52f7407
Ben Firth <bfirth93@gmail.com>
2020-01-10 11:43:47 -0700

    Add image coming soon and button for favs

--- src/components/Truck/Truck.js
+++ src/components/Truck/Truck.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Truck.scss';
+import imagecomingsoon from '../../styles/imagecomingsoon.png';
 
 const Truck = ({ name, desc, image }) => {
   // image ? console.log(image.logo) : null
@@ -11,8 +12,9 @@ const Truck = ({ name, desc, image }) => {
     <article className='truck-card'>
       <h4>{name}</h4>
       <hr/>
-      {image ? <img src={image.logo} alt='Food truck logo' /> : null}
+      {image ? <img src={image.logo} alt='Food truck logo' height='100' width='170' /> : <img src={imagecomingsoon} alt='Food truck logo' height='100' width='170' />}
       <p>{desc}</p>
+      <button>ADD TO POTENTIAL LOCATIONS</button>
     </article>
   )
 }
