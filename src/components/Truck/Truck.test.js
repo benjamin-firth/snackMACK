28c58a7c47a0ef95ccce161983a918d497820e3a
Ben Firth <bfirth93@gmail.com>
2020-01-10 11:15:48 -0700

    Add truck test structure and snapshot

---
+++ src/components/Truck/Truck.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Truck from './Truck';
+
+describe('Truck', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow( <Truck />)
+  })
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
+
+})
