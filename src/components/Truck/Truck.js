60b6d95357e238dcc1ee2f92decdeab41886f189
Ben Firth <bfirth93@gmail.com>
2020-01-10 07:38:50 -0700

    Add pictures and lat long to truck card

--- src/components/Truck/Truck.js
+++ src/components/Truck/Truck.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import './Truck.scss';
 
-const Truck = ({ name, desc }) => {
+const Truck = ({ name, desc, image }) => {
+  // image ? console.log(image.logo) : null
+  if (image) {
+    console.log(image.logo)
+  }
 
   return (
-    <article>
+    <article className='truck-card'>
       <h4>{name}</h4>
+      <hr/>
+      {image ? <img src={image.logo} alt='Food truck logo' /> : null}
       <p>{desc}</p>
     </article>
   )
