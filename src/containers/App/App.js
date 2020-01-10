7ed0b5a11c50e40647923ad3fa0209843a9a8571
Ben Firth <bfirth93@gmail.com>
2020-01-10 11:16:17 -0700

    Add app test structure and snapshot

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component} from 'react';
-import { Route, NavLink } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import './App.scss';
 import Login from '../Login/Login';
 import TrucksContainer from '../../components/TrucksContainer/TrucksContainer';
