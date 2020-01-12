163c62a283d364fd2d57c6c1c13a04af70494a0e
Ben Firth <bfirth93@gmail.com>
2020-01-12 15:13:55 -0700

    Create city data file

---
+++ src/utils/cityData.js
@@ -0,0 +1,14 @@
+export const cityData = {
+  calgary: {lat: 51.0, long: -114.0},
+  edmonton: {lat: 53.5, long: -113.5},
+  halifax: {lat: 44.6, long: -63.6},
+  hamilton: {lat: 43.3, long: -80.0},
+  ottawa: {lat: 45.4, long: -75.7},
+  toronto: {lat: 43.7, long: -79.4},
+  vancouver: {lat: 49.3, long: -123.1},
+  victoria: {lat: 37.5, long: -144.8},
+}
+
+
+
+
