3ed67bda064474fb40d7eb10c0b9a1a0d300fa5d
Ben Firth <bfirth93@gmail.com>
2020-01-12 11:12:14 -0700

    Add to mockdata

---
+++ src/utils/mockData.js
@@ -0,0 +1,64 @@
+export const mockPotentialLocations = [{
+  desc: "Our food trailer is designed for efficiently producing quality comfort food! We serve baked potatoes loaded with a variety of toppings! BBQ Pulled Pork Tex Mex Mushroom Swiss Classic English Sheppard's Pie",
+  image: undefined,
+  location: {
+    display: "Prairie Horizon Pop Up Market",
+    latitude: 51.19624517,
+    longitude: -113.99491753,
+    time: 1578762000
+  },
+  name: "Baked And Loaded"
+}, 
+{
+  desc: "Our food trailer is designed for efficiently producing quality comfort food! We serve baked potatoes loaded with a variety of toppings! BBQ Pulled Pork Tex Mex Mushroom Swiss Classic English Sheppard's Pie",
+  image: undefined,
+  location: {
+    display: "Prairie Horizon Pop Up Market",
+    latitude: 51.19624517,
+    longitude: -113.99491753,
+    time: 1578762000
+  },
+  name: "Test Food Truck 2"
+}]
+
+export const mockFilteredPotentialLocations = {
+  desc: "Our food trailer is designed for efficiently producing quality comfort food! We serve baked potatoes loaded with a variety of toppings! BBQ Pulled Pork Tex Mex Mushroom Swiss Classic English Sheppard's Pie",
+  image: undefined,
+  location: {
+    display: "Prairie Horizon Pop Up Market",
+    latitude: 51.19624517,
+    longitude: -113.99491753,
+    time: 1578762000
+  },
+  name: "Baked And Loaded"
+}
+
+export const mockAddPotentialLocationActionObj = {
+  type: 'ADD_POTENTIAL_LOCATION',
+  potentialLocation: {
+    desc: "Our food trailer is designed for efficiently producing quality comfort food! We serve baked potatoes loaded with a variety of toppings! BBQ Pulled Pork Tex Mex Mushroom Swiss Classic English Sheppard's Pie",
+    image: undefined,
+    location: {
+      display: "Prairie Horizon Pop Up Market",
+      latitude: 51.19624517,
+      longitude: -113.99491753,
+      time: 1578762000
+    },
+    name: "Baked And Loaded"
+  }
+}
+
+export const mockRemovePotentialLocationActionObj = {
+  type: 'REMOVE_POTENTIAL_LOCATION',
+  potentialLocation: {
+    desc: "Our food trailer is designed for efficiently producing quality comfort food! We serve baked potatoes loaded with a variety of toppings! BBQ Pulled Pork Tex Mex Mushroom Swiss Classic English Sheppard's Pie",
+    image: undefined,
+    location: {
+      display: "Prairie Horizon Pop Up Market",
+      latitude: 51.19624517,
+      longitude: -113.99491753,
+      time: 1578762000
+    },
+    name: "Baked And Loaded"
+  }
+}
