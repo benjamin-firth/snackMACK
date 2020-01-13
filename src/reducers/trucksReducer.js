508c2080b38ec2ef24288e6ca81bf1221b7beb99
Ben Firth <bfirth93@gmail.com>
2020-01-12 19:23:18 -0700

    Add toggle potential location to trucks reducer

--- src/reducers/trucksReducer.js
+++ src/reducers/trucksReducer.js
@@ -2,6 +2,13 @@ export const trucksReducer = (state = [], action) => {
   switch (action.type) {
     case 'SET_TRUCKS':
       return action.trucks;
+    case 'TOGGLE_POTENTIAL_LOCATION':
+      return [...state].map(truck => {
+        if (truck.name === action.truck.name) {
+          return { ...truck, isPotentialLocation: !truck.isPotentialLocation }
+        }
+        return truck;
+      });
     default:
       return state;
   }
