aa49ab4b44b325258aa324877fb3d6b0cb483a7d
Ben Firth <bfirth93@gmail.com>
2020-01-12 19:22:53 -0700

    Add div for styling to login

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -28,24 +28,26 @@ export class Login extends Component {
 
   render() {
     return (
-      <div className='login-container'>
-        <section className='login'>
-          <h1>snackMACK</h1>
-          <h3>“The food truck locator app to bring your dates to the next level!”</h3>
-          <form>
-            <select onChange={(e) => this.handleChange(e.target.value)}>
-              <option value='calgary'>Calgary</option>
-              <option value='edmonton'>Edmonton</option>
-              <option value='halifax'>Halifax</option>
-              <option value='hamilton'>Hamilton</option>
-              <option value='ottawa'>Ottawa</option>
-              <option value='toronto'>Toronto</option>
-              <option value='vancouver'>Vancouver</option>
-              <option value='victoria'>Victoria</option>
-            </select>
-            <button type='button' onClick={this.handleSubmit}>SUBMIT</button>
-          </form>
-        </section>
+      <div className='background-image'>
+        <div className='login-container'>
+          <section className='login'>
+            <h1>snackMACK</h1>
+            <h3>“The food truck locator app to bring your dates to the next level!”</h3>
+            <form>
+              <select onChange={(e) => this.handleChange(e.target.value)}>
+                <option value='calgary'>Calgary</option>
+                <option value='edmonton'>Edmonton</option>
+                <option value='halifax'>Halifax</option>
+                <option value='hamilton'>Hamilton</option>
+                <option value='ottawa'>Ottawa</option>
+                <option value='toronto'>Toronto</option>
+                <option value='vancouver'>Vancouver</option>
+                <option value='victoria'>Victoria</option>
+              </select>
+              <button type='button' onClick={this.handleSubmit}>SUBMIT</button>
+            </form>
+          </section>
+        </div>
       </div>
     )
   }
