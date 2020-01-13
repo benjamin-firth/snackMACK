932678805f275657f0a58c8cb8c59af7b2ecbb14
Ben Firth <bfirth93@gmail.com>
2020-01-13 12:13:49 -0700

    Add login testing

--- src/containers/Login/Login.test.js
+++ src/containers/Login/Login.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Login } from './Login';
+import { Login, mapDispatchToProps } from './Login';
+import * as mockData from '../../utils/mockData';
 
 describe('Login', () => {
   let wrapper;
@@ -13,4 +14,63 @@ describe('Login', () => {
     expect(wrapper).toMatchSnapshot();
   })
 
+  describe('handleChange', () => {
+    it('should run handleChange when the select is changed', () => {
+      wrapper.instance().handleChange = jest.fn();
+      const mockEvent = {
+        preventDefault: jest.fn(),
+        target: {
+          value: 'canada test'
+        }
+      };
+  
+      wrapper.find('select').simulate('change', mockEvent);
+      expect(wrapper.instance().handleChange).toHaveBeenCalled();
+    })
+  
+    it('should update state when handleChange is called', () => {
+      const mockState = {
+        city: 'canada',
+        lat: 40.0,
+        long: 120.0,
+      };
+      const expected = {
+        city: 'calgary',
+        lat: 51.0,
+        long: -114.0,
+      }
+      wrapper.setState(mockState);
+      wrapper.instance().handleChange('calgary');
+  
+      expect(wrapper.state()).toEqual(expected);
+    })
+  })
+
+  describe('handleSubmit', () => {
+    let history;
+    let mockhistory;
+    beforeEach(() => {
+      mockhistory = jest.fn()
+      history = { push: mockhistory }
+    })
+
+  it('should update the page when this.props.history.push is used', () => {
+    const chooseCity = jest.fn();
+    wrapper = shallow(<Login history={history} chooseCity={chooseCity}/>);
+    wrapper.find('button').simulate('click');
+    expect(mockhistory).toHaveBeenCalledWith('/main');
+    })
+  })
+
+  describe('mapDispatchToProps', () => {
+    const mockDispatch = jest.fn();
+
+    it('should dispatch chooseCity when handleSubmit', () => {
+      const { city, lat, long } = mockData.mockCity;
+      const mappedProps = mapDispatchToProps(mockDispatch);
+
+      mappedProps.chooseCity(city, lat, long);
+      expect(mockDispatch).toHaveBeenCalledWith(mockData.mockCityActionObj);
+    })
+  })
 })
