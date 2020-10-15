1f0ed8b9b7a248a893220bf201cc6680990ff725
Ben Firth <bfirth93@gmail.com>
2020-10-15 14:34:30 -0600

    Change login to hooks

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,65 +1,56 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from "react-router-dom";
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import { chooseCity } from '../../actions';
 import { cityData } from '../../utils/cityData';
 import './Login.scss';
 
-export class Login extends Component {
-  constructor() {
-    super();
-    this.state = {
-      city: 'calgary',
-      lat: 51.0,
-      long: -114.0
-    }
+export const Login = () => {
+  const [city, setCity] = useState('calgary');
+  const [lat, setLat] = useState(51.0);
+  const [long, setLong] = useState(-114.0);
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const handleChange = (newCity) => {
+    setCity(newCity);
+    setLat(cityData[city].lat);
+    setLong(cityData[city].long);
   }
 
-  handleChange = (city) => {
-    this.setState({ city })
-    const lat = cityData[city].lat;
-    const long = cityData[city].long;
-    this.setState({ lat, long });
+  const handleSubmit = () => {
+    dispatch(chooseCity(city, lat, long));
+    history.push('/main');
   }
 
-  handleSubmit = () => {
-    this.props.chooseCity(this.state.city, this.state.lat, this.state.long);
-    this.props.history.push('/main');
-  }
-
-  render() {
-    return (
-      <div className='background-image'>
-        <div className='login-container'>
-          <section className='login'>
-            <h1>snackMACK</h1>
-            <h3>“The food truck locator app to bring your dates to the next level!”</h3>
-            <form>
-              <select onChange={(e) => this.handleChange(e.target.value)}>
-                <option value='calgary'>Calgary</option>
-                <option value='edmonton'>Edmonton</option>
-                <option value='halifax'>Halifax</option>
-                <option value='hamilton'>Hamilton</option>
-                <option value='ottawa'>Ottawa</option>
-                <option value='toronto'>Toronto</option>
-                <option value='vancouver'>Vancouver</option>
-                <option value='victoria'>Victoria</option>
-              </select>
-              <button type='button' onClick={this.handleSubmit}>SUBMIT</button>
-            </form>
-          </section>
-        </div>
+  return (
+    <div className='background-image'>
+      <div className='login-container'>
+        <section className='login'>
+          <h1>snackMACK</h1>
+          <h3>“The food truck locator app to bring your dates to the next level!”</h3>
+          <form>
+            <select onChange={(e) => handleChange(e.target.value)}>
+              <option value='calgary'>Calgary</option>
+              <option value='edmonton'>Edmonton</option>
+              <option value='halifax'>Halifax</option>
+              <option value='hamilton'>Hamilton</option>
+              <option value='ottawa'>Ottawa</option>
+              <option value='toronto'>Toronto</option>
+              <option value='vancouver'>Vancouver</option>
+              <option value='victoria'>Victoria</option>
+            </select>
+            <button type='button' onClick={handleSubmit}>SUBMIT</button>
+          </form>
+        </section>
       </div>
-    )
-  }
+    </div>
+  )
 }
-
-export const mapDispatchToProps = dispatch => ({
-  chooseCity: (city, lat, long) => dispatch(chooseCity(city, lat, long))
-})
 
 Login.propTypes = {
   chooseCity: PropTypes.func
 }
 
-export default connect(null, mapDispatchToProps)(Login);
+export default Login;
